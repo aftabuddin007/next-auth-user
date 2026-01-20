9e9f91b4f6a63fed0a767db05eb3816684e21ac7
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-20 23:59:57 +0600

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='border-b-2 py-3 text-center space-x-6'>
+            
+        </div>
+    );
+};
+
+export default Navbar;
