e967cc186a9705834e352524ad1d3fb4bccf420b
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-21 17:14:32 +0600

    add forbidden route

---
+++ src/app/forbidden/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Forbidden = () => {
+    return (
+        <div>
+            <h2 className="py-20 text-center text-5xl font-bold">Forbidden</h2>
+        </div>
+    );
+};
+
+export default Forbidden;
--- src/proxy.js
+++ src/proxy.js
@@ -2,15 +2,17 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
-const privateRoutes = ["/private","/dashboard","/secret"]
+const privateRoutes = ["/private","/secret"]
+const adminRoutes = ['/dashboard']
 export async function proxy(req) {
 const token = await getToken({req})
 const reqPath = req.nextUrl.pathname
 const isAuthenticated = Boolean(token);
 const isUser = token?.role == "user";
 const isAdmin = token?.role == "admin";
-const isPrivate = privateRoutes.some((route)=>reqPath.startsWith(route))
 
+const isPrivate = privateRoutes.some((route)=>reqPath.startsWith(route))
+const isAdminRoute = adminRoutes.some((route)=>reqPath.startsWith(route))
 if(!isAuthenticated && isPrivate){
     
     const loginUrl = new URL('/api/auth/signin', req.url)
@@ -18,9 +20,15 @@ if(!isAuthenticated && isPrivate){
     return NextResponse.redirect(loginUrl)
 }
 
-console.log({isAuthenticated,isUser,reqPath,isPrivate})
+if(!isAuthenticated && !isAdmin && isAdminRoute){
+  return NextResponse.redirect(new URL('/forbidden',req.url))
+}
 
-console.log(token)
+
+
+// console.log({isAuthenticated,isUser,reqPath,isPrivate})
+
+// console.log(token)
 
 
 
